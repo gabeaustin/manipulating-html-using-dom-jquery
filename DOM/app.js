22e5377ae375de455fe85473d7a072ca099f56bb
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-03 13:08:55 -0600

    removed comments from alert boxes

--- DOM/app.js
+++ DOM/app.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let container = document.createElement("container");
     let divForNumTwo = document.createElement("div");
     let divForNumThree = document.createElement("div");
     let divForNumFour = document.createElement("div");
@@ -9,39 +8,37 @@ document.addEventListener("DOMContentLoaded", function () {
     let button = document.createElement("button");
     let userInputBtn = document.getElementById("displayUserInputBtn");
     let buttonText = document.createTextNode("Click Me!");
+    let randomColorPara = document.createElement("p");
 
     button.appendChild(buttonText);
-    document.body.appendChild(container);
     document.body.appendChild(mainDiv);
     document.body.appendChild(button);
+    
+    document.body.appendChild(randomColorPara);
     mainDiv.appendChild(button);
-    // mainDiv.appendChild(divForNumOne);
     mainDiv.appendChild(divForNumTwo);
     mainDiv.appendChild(divForNumThree);
     mainDiv.appendChild(divForNumFour);
     mainDiv.appendChild(divForNumFive);
     mainDiv.appendChild(divForNumSix);
+    // mainDiv.body.appendChild(randomColorPara);
 
-
-
-    // Num 2
-    userInputBtn.addEventListener("click", function () {
-        // change to an alert
-        // alert(userText.value); // remove this line before deploying
-        console.log(userText.value);
-    })
-
-    let divForNumOne = document.createElement("div");
     let numOne = document.getElementById("num1");
     button.addEventListener("click", function () {
-        // alert(userText.value);
-        console.log("nice message");
+        alert("Here is a nice message!! Yeah me!");
+        // console.log("nice message");
     })
     mainDiv.appendChild(numOne);
 
 
     let numTwo = document.getElementById("num2");
     mainDiv.appendChild(numTwo);
+    
+    userInputBtn.addEventListener("click", function () {
+        // change to an alert
+        alert(userText.value); // remove this line before deploying
+        // console.log(userText.value);
+    })
 
 
     let numThree = document.getElementById("num3");
@@ -57,6 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     let numFour = document.getElementById("num4");
+    // numFour.appendChild(randomColorPara);
     mainDiv.appendChild(numFour);
 
     let numFive = document.getElementById("num5");
