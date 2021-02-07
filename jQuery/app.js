530f0a923b0818d1843d2cefea3876082cf420cf
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-07 12:38:06 -0600

    completed numTwo -> user types in text box and presses submit btn. the value of text box is show in alert

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
+    // numOne
     let buttonForNumOne = $("<button>Click Me</button>");
 
     $("#numOne").append(buttonForNumOne);
     $("#numOne").click(function() {
         alert("This is my nice messge...\nhehehehe");
+    })
+
+    // numTwo
+    $("#buttonForNumTwo").click(function() {
+        console.log($("#someTextInputBox").val());
     })
 
 
